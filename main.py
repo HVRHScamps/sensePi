5d1910ebd4c08286d757234fdbd705b4a15386de
cannotilever <51347140+cannotilever@users.noreply.github.com>
2022-06-26 11:17:50 -0400

    get rid of gyroscope

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ while True:
     if time.time() - clock >= 0.33:
         hat.load_image("pixart/{}.png".format(table.getString("display", "dead")))
         clock = time.time()
-    orientation = hat.get_orientation_degrees()
-    for axis in ["pitch", "roll", "yaw"]:
-        # weird format to comply with libhousy (updating it on each laptop would be a pain)
-        table.putNumber("sh"+axis.title(), orientation[axis])
     acceleration = hat.get_accelerometer_raw()
     table.putNumberArray("shAccel", [acceleration["x"], acceleration["y"], acceleration["z"]])
     time.sleep(0.02)
